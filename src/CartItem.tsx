2a01d7fa5f394d2d3945a824ddb44559b9cb54b7
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-03-21 21:53:59 -0300

    reference

--- src/CartItem.tsx
+++ src/CartItem.tsx
@@ -11,8 +11,10 @@ function CartItem({ cartItem: { id, name, price, count } }: { cartItem: CartItem
             <h2>Price: {(price * count).toFixed(2)}</h2>
             <button className="item-btn background-red" onClick={() => updateCart({ intent: "remove", id })}>Remove</button>
             <div className="d-flex d-flex-row gap-2 align-items-center justify-content-center">
+                {/* Remove icon from google fonts: https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aremove%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024 */}
                 <button className="item-btn" onClick={() => updateCart({ intent: "decrement", id })}><span className="material-symbols-outlined">remove</span></button>
                 <p>{count}</p>
+                {/* Add icon from google fonts: https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aadd%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024 */}
                 <button className="item-btn" onClick={() => updateCart({ intent: "increment", id })}><span className="material-symbols-outlined">add</span></button>
             </div>
         </div>
--- src/StoreItem.tsx
+++ src/StoreItem.tsx
@@ -9,6 +9,7 @@ function StoreItem({ storeItem: { id, name, price } }: { storeItem: Item }) {
         <div className="d-flex d-flex-col align-items-center justify-content-center text-center">
             <h1>{name}</h1>
             <h2>Price: ${price}</h2>
+            {/* Add icon from google fonts: https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aadd%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024 */}
             <button className="item-btn background-green text-center" onClick={() => updateCart({ intent: "add", id })}><span className="material-symbols-outlined">add</span></button>
         </div>
     )
